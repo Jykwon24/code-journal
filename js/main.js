8fd6373fe56234384e3dbb666743801dabc2fc7c
Jeff Kwon <jykwon007@gmail.com>
2021-11-30 22:12:49 -0800

    Listener for 'submit' event, add to obj, task 6a.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 /* global data */
 /* exported data */
 
+var $journalObj = {};
 var $photoUrl = document.getElementById('photo-url');
 var $img = document.querySelector('.img-container');
 
 $photoUrl.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
+});
+
+var $form = document.getElementById('form-entry');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  $journalObj.title = $form.elements.title.value;
+  $journalObj.photoUrl = $form.elements.photoUrl.value;
+  $journalObj.notes = $form.elements.notes.value;
+  $form.reset();
 });
