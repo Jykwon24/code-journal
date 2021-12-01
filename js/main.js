f3f1bc2ad459c1f2c50e3ee68c936e8b8080d1a6
Jeff Kwon <jykwon007@gmail.com>
2021-11-30 20:37:26 -0800

    Listen for inputs on photoURL, task5

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.getElementById('photo-url');
+var $img = document.querySelector('.img-container');
+
+$photoUrl.addEventListener('input', function (event) {
+  $img.setAttribute('src', event.target.value);
+});
