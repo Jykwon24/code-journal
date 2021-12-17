ae85d03ad81f8a7949ab4e55a95c867b8c01fa63
Jeff Kwon <jykwon007@gmail.com>
2021-12-16 20:46:35 -0800

    Task 7-11, edit entry object and replace the existing.

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ $photoUrl.addEventListener('input', function (event) {
 });
 
 function journalEntry(entry) {
-  var $containerDiv = document.createElement('div');
+  var $containerDiv = document.createElement('li');
+  $containerDiv.setAttribute('id', entry.entryId);
   $containerDiv.setAttribute('class', 'new-row');
   $containerDiv.setAttribute('data-entry-id', entry.entryId);
 
@@ -28,9 +29,10 @@ function journalEntry(entry) {
   $entryImg.setAttribute('src', entry.photoUrl);
 
   var $entryTitle = document.createElement('h2');
-  var $editIcon = document.createElement('i');
+  var $editIcon = document.createElement('a');
   $entryTitle.setAttribute('class', 'list-style');
   $editIcon.setAttribute('class', 'fas fa-pen edit-button');
+  $editIcon.setAttribute('href', '#');
   $editIcon.setAttribute('data-entry-id', entry.entryId);
   var $titleTxt = document.createTextNode(entry.title);
   $entryTitle.appendChild($titleTxt);
@@ -60,37 +62,33 @@ var $form = document.getElementById('form-entry');
 $form.addEventListener('submit', function (event) {
   var journalObj = {};
   event.preventDefault();
-  journalObj.title = $form.elements.title.value;
-  journalObj.photoUrl = $form.elements.photoUrl.value;
-  journalObj.notes = $form.elements.notes.value;
-  journalObj.entryId = data.nextEntryId++;
-  data.entries.unshift(journalObj);
-  data.view = 'entries';
-  prependToList(journalObj);
-  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $formContainer.className = 'hidden';
-  $entryList.className = 'new-row container';
-  $entries.className = 'hidden';
-  $form.reset();
-});
-
-document.addEventListener('DOMContentLoaded', function (event) {
-  for (var i = 0; i < data.entries.length; i++) {
-    var result = journalEntry(data.entries[i]);
-    $ul.appendChild(result);
-  }
-  if (data.view === 'entries') {
+  if (data.editing === null) {
+    journalObj.title = $form.elements.title.value;
+    journalObj.photoUrl = $form.elements.photoUrl.value;
+    journalObj.notes = $form.elements.notes.value;
+    journalObj.entryId = data.nextEntryId++;
+    data.entries.unshift(journalObj);
+    data.view = 'entries';
+    prependToList(journalObj);
+    $img.setAttribute('src', 'images/placeholder-image-square.jpg');
     $formContainer.className = 'hidden';
     $entryList.className = 'new-row container';
-  } else {
-    $entryList.className = 'hidden';
-    $formContainer.className = 'container';
-  }
-  if (data.entries.length > 0) {
     $entries.className = 'hidden';
+    $form.reset();
   } else {
-    $entries.className = 'container entries';
+    data.editing.title = $form.elements.title.value;
+    data.editing.photoUrl = $form.elements.photoUrl.value;
+    data.editing.notes = $form.elements.notes.value;
+    var $editLi = document.getElementById('' + data.editing.entryId);
+    $editLi.replaceWith(journalEntry(data.editing));
+    data.view = 'entries';
+    $formContainer.className = 'hidden';
+    $entryList.className = 'new-row container';
+    $entries.className = 'hidden';
+    data.editing = null;
+    $form.reset();
   }
+
 });
 
 var $navHeader = document.querySelector('.entry-header');
@@ -114,6 +112,7 @@ $newButton.addEventListener('click', function (event) {
   $entryList.className = 'hidden';
   $formContainer.className = 'container';
   data.view = 'entry-form';
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
 });
 
 $ul.addEventListener('click', function (event) {
@@ -128,8 +127,35 @@ $ul.addEventListener('click', function (event) {
   for (i = 0; i < data.entries.length; i++) {
     var dataArr = data.entries[i].entryId;
     if ($entryNumOnClick === dataArr) {
-      data.editing = $entryNumOnClick;
+      data.editing = data.entries[i];
+      var $titleTextEdit = document.getElementById('title');
+      $titleTextEdit.value = data.entries[i].title;
+      var $imgEdit = document.getElementById('photo-url');
+      var $imgBox = document.getElementById('img-box');
+      $imgBox.setAttribute('src', data.entries[i].photoUrl);
+      $imgEdit.value = data.entries[i].photoUrl;
+      var $notesEdit = document.getElementById('notes');
+      $notesEdit.value = data.entries[i].notes;
     }
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var result = journalEntry(data.entries[i]);
+    $ul.appendChild(result);
+  }
+  if (data.view === 'entries') {
+    $formContainer.className = 'hidden';
+    $entryList.className = 'new-row container';
+  } else {
+    $entryList.className = 'hidden';
+    $formContainer.className = 'container';
+  }
+  if (data.entries.length > 0) {
+    $entries.className = 'hidden';
+  } else {
+    $entries.className = 'container entries';
   }
 });
 
